c60c8cb073910c702ccb5cc84907790ad9fc4029
Marcos Vinicius <hello@marvig.dev>
2022-01-12 11:14:51 -0300

    Resolução - Linters

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ function handleLogin(e) {
 
   // https://stackoverflow.com/questions/588263/how-can-i-get-all-a-forms-values-that-would-be-submitted-without-submitting
   const formData = new FormData(e.target);
-  const email = formData.get("email");
-  const password = formData.get("password");
+  const email = formData.get('email');
+  const password = formData.get('password');
 
-  if (email !== "tryber@teste.com" || password !== "123456") {
-    return alert("Email ou senha inválidos.");
+  if (email !== 'tryber@teste.com' || password !== '123456') {
+    return alert('Email ou senha inválidos.');
   }
 
-  return alert("Olá, Tryber!");
+  return alert('Olá, Tryber!');
 }
 
-const loginForm = document.getElementById("trybewarts-login");
-loginForm.addEventListener("submit", handleLogin);
+const loginForm = document.getElementById('trybewarts-login');
+loginForm.addEventListener('submit', handleLogin);
 
 // Lógica de habilitar e desabilitar botão de envio de formulário
 function handleAgreementCheckbox() {
-  const agreementCheckbox = document.getElementById("agreement");
-  const submitButton = document.getElementById("submit-btn");
+  const agreementCheckbox = document.getElementById('agreement');
+  const submitButton = document.getElementById('submit-btn');
 
   if (agreementCheckbox.checked) {
     submitButton.disabled = false;
@@ -29,21 +29,21 @@ function handleAgreementCheckbox() {
   }
 }
 
-const agreementCheckbox = document.getElementById("agreement");
-agreementCheckbox.addEventListener("change", handleAgreementCheckbox);
+const agreementCheckbox = document.getElementById('agreement');
+agreementCheckbox.addEventListener('change', handleAgreementCheckbox);
+
+const textArea = document.getElementById('textarea');
 
 function updateNumber() {
-  let valor;
-  valor = textArea.value.length;
+  const valor = textArea.value.length;
   console.log(valor);
-  const counter = document.getElementById("counter");
+  const counter = document.getElementById('counter');
   counter.innerHTML = 500 - valor;
 }
 
-const textArea = document.getElementById("textarea");
-textArea.addEventListener("keyup", updateNumber);
+textArea.addEventListener('keyup', updateNumber);
 
-const mainForm = document.getElementById("evaluation-form");
+const mainForm = document.getElementById('evaluation-form');
 
 // Lógica de envio do formulário principal
 function handleFormSubmit(e) {
@@ -51,23 +51,20 @@ function handleFormSubmit(e) {
   const formData = new FormData(e.target);
 
   // https://stackoverflow.com/questions/8563240/how-to-get-all-checked-checkboxes/31113246
-  const selected = document.querySelectorAll(
-    `input[name="want-to-learn"]:checked`
-  );
-  let subjectList = [];
-  for (let i = 0; i < selected.length; i++) {
-    subjectList.push(selected[i].value);
-  }
+  const selected = [];
+  document.querySelectorAll('input[name="want-to-learn"]:checked').forEach((v) => {
+    selected.push(v.value);
+  });
 
   mainForm.innerHTML = `
-    <p>Nome: ${formData.get("name")} ${formData.get("surname")}</p>
-    <p>Email: ${formData.get("email")}</p>
-    <p>Casa: ${formData.get("house")}</p>
-    <p>Família: ${formData.get("family")}</p>
-    <p>Matérias: ${subjectList.join(", ")}</p>
-    <p>Avaliação: ${formData.get("rate")}</p>
-    <p>Observações: ${formData.get("obs")}</p>
+    <p>Nome: ${formData.get('name')} ${formData.get('surname')}</p>
+    <p>Email: ${formData.get('email')}</p>
+    <p>Casa: ${formData.get('house')}</p>
+    <p>Família: ${formData.get('family')}</p>
+    <p>Matérias: ${selected.join(', ')}</p>
+    <p>Avaliação: ${formData.get('rate')}</p>
+    <p>Observações: ${formData.get('obs')}</p>
   `;
 }
 
-mainForm.addEventListener("submit", handleFormSubmit);
+mainForm.addEventListener('submit', handleFormSubmit);
