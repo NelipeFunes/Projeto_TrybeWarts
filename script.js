8d2975bacbe2eea510b9e10e2c91a5b061c63977
Marcos Vinicius <hello@marvig.dev>
2022-01-12 11:03:38 -0300

    Resolução - Requisito 21

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const loginForm = document.getElementById("trybewarts-login");
 loginForm.addEventListener("submit", handleLogin);
 
 // Lógica de habilitar e desabilitar botão de envio de formulário
-function handleSubmitButton() {
+function handleAgreementCheckbox() {
   const agreementCheckbox = document.getElementById("agreement");
   const submitButton = document.getElementById("submit-btn");
 
@@ -30,15 +30,44 @@ function handleSubmitButton() {
 }
 
 const agreementCheckbox = document.getElementById("agreement");
-agreementCheckbox.addEventListener("change", handleSubmitButton);
+agreementCheckbox.addEventListener("change", handleAgreementCheckbox);
 
 function updateNumber() {
   let valor;
   valor = textArea.value.length;
   console.log(valor);
-  const counter = document.getElementById('counter');
-  counter.innerHTML = 500-valor;
+  const counter = document.getElementById("counter");
+  counter.innerHTML = 500 - valor;
 }
 
-const textArea = document.getElementById('textarea');
-textArea.addEventListener('keyup', updateNumber);
+const textArea = document.getElementById("textarea");
+textArea.addEventListener("keyup", updateNumber);
+
+const mainForm = document.getElementById("evaluation-form");
+
+// Lógica de envio do formulário principal
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+
+  // https://stackoverflow.com/questions/8563240/how-to-get-all-checked-checkboxes/31113246
+  const selected = document.querySelectorAll(
+    `input[name="want-to-learn"]:checked`
+  );
+  let subjectList = [];
+  for (let i = 0; i < selected.length; i++) {
+    subjectList.push(selected[i].value);
+  }
+
+  mainForm.innerHTML = `
+    <p>Nome: ${formData.get("name")} ${formData.get("surname")}</p>
+    <p>Email: ${formData.get("email")}</p>
+    <p>Casa: ${formData.get("house")}</p>
+    <p>Família: ${formData.get("family")}</p>
+    <p>Matérias: ${subjectList.join(", ")}</p>
+    <p>Avaliação: ${formData.get("rate")}</p>
+    <p>Observações: ${formData.get("obs")}</p>
+  `;
+}
+
+mainForm.addEventListener("submit", handleFormSubmit);
